2a0269e8434e003ceed5bb3d913304dc435e44c6
siarhkul <siarhkul@gmail.com>
2024-07-19 16:29:18 +0200

    chore: Delete redundant code

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TransactionDto } from './dto/transaction.dto';
 
 @Controller('transaction')
@@ -6,16 +6,5 @@ export class TransactionController {
   @Post()
   create(@Body() body: TransactionDto) {
     return { ...body, some: 'text1' };
-  }
-
-  @Put()
-  update(@Body() body: TransactionDto) {
-    //todo: how to include id
-    return body;
-  }
-
-  @Delete('/:id')
-  delete(@Param('id') id: string) {
-    return id;
   }
 }
