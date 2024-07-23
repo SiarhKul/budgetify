af3a5d9bdb2995d9e5d4cd673489a8653245d805
siarhkul <siarhkul@gmail.com>
2024-07-23 21:34:24 +0200

    chore: Add password and user mask

--- config/config.mongo.ts
+++ config/config.mongo.ts
@@ -1,12 +1,18 @@
 import type { ConfigService } from '@nestjs/config';
 import { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
+import { Logger } from '@nestjs/common';
 
 export const useFactoryReturn = (
   configService: ConfigService,
 ): MongooseModuleFactoryOptions => {
+  const logger = new Logger('ConfigModule');
+
   const mongoDbUsername = configService.get<string>('MONGODB_USERNAME');
   const mongoDbPassword = configService.get<string>('MONGODB_PASSWORD');
   const uri = `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@aducationscourses.a4vyllo.mongodb.net/?retryWrites=true&w=majority&appName=AducationsCourses`;
+  const maskedUri = uri.replace(/\/\/(.*):(.*)@/, '//*****:*****@');
+
+  logger.log(`MongoDB connection string: ${maskedUri}`);
 
   return {
     uri,
