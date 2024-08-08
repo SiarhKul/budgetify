b4cd942d11ec343b905aacc4ad86034816bdd5b0
siarhkul <siarhkul@gmail.com>
2024-08-08 22:56:15 +0200

    chore: Update config service

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,37 +6,32 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import getParams from '../../config/config.parameters';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  private params = getParams();
-
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
     const token: string | undefined = this.extractTokenFromHeader(request);
-    console.log('=>(auth.guard.ts:20) S', token);
+    const jwtSecret = this.configService.get<string | undefined>('jwtSecret');
 
     if (!token) {
-      console.log(1);
       throw new UnauthorizedException();
     }
 
     try {
-      console.log(2);
-
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: this.params.jwtSecret,
+        secret: jwtSecret,
       });
-      // console.log('=>(auth.guard.ts:29) payload', payload);
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
-      // request['user'] = payload;
+
+      request['user'] = payload;
     } catch (e) {
-      console.log('3333333333333333333', e);
-      console.log(3);
       throw new UnauthorizedException(e.message);
     }
 
