2604bbd0b7647f9e2e6c00f39836bafe9ebeabcf
siarhkul <siarhkul@gmail.com>
2024-08-18 21:27:13 +0200

    chore: Init statistic

---
+++ src/statistic/statistic.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class StatisticService {
+  getStatisticByDate() {
+    return 'This action returns all statistic by date';
+  }
+}
