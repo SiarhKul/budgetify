5c3d41a727642518d9f314ca96c8ee9109bc8d82
siarhkul <siarhkul@gmail.com>
2024-08-19 22:46:15 +0200

    chore: Enhance the group logic

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Transaction } from '../schemas/transaction.schema';
-import { Model } from 'mongoose';
+import { Aggregate, Model } from 'mongoose';
+import { CategoriesStatistic } from '../ts/statistic/statistic.interface';
 
 @Injectable()
 export class StatisticService {
@@ -10,29 +11,34 @@ export class StatisticService {
     private readonly transactionModel: Model<Transaction>,
   ) {}
 
-  getStatisticByDate(startDate: Date, endDate: Date) {
-    return this.transactionModel.aggregate([
-      {
-        $match: {
-          paymentDate: {
-            $gte: new Date(startDate),
-            $lte: new Date(endDate),
+  getStatisticByDate(
+    startDate: Date,
+    endDate: Date,
+  ): Promise<CategoriesStatistic[]> {
+    return this.transactionModel
+      .aggregate([
+        {
+          $match: {
+            paymentDate: {
+              $gte: new Date(startDate),
+              $lte: new Date(endDate),
+            },
           },
         },
-      },
-      {
-        $group: {
-          _id: '$categories',
-          totalAmount: { $sum: '$amount' },
+        {
+          $group: {
+            _id: '$categories',
+            totalAmount: { $sum: '$amount' },
+          },
         },
-      },
-      {
-        $project: {
-          _id: 0,
-          category: '$_id',
-          totalAmount: 1,
+        {
+          $project: {
+            _id: 0,
+            category: '$_id',
+            totalAmount: 1,
+          },
         },
-      },
-    ]);
+      ])
+      .exec();
   }
 }
---
+++ src/ts/statistic/statistic.interface.ts
@@ -0,0 +1,4 @@
+export interface CategoriesStatistic {
+  name: string;
+  value: number;
+}
