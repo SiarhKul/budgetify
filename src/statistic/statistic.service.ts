c79d83d8d943c287df39b40a70911cd11c96a1ae
siarhkul <siarhkul@gmail.com>
2024-08-22 08:42:59 +0200

    chore: Add monthly statistic

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -7,6 +7,7 @@ import {
   CategorizedAmountsUnder,
 } from '../ts/statistic/statistic.interface';
 import { TransactionType } from '../ts/transactons/transactions.enums';
+import { RetrieveMonthlyStatisticDto } from './dto/retrieve-monthly-statistic.dto';
 
 @Injectable()
 export class StatisticService {
@@ -51,6 +52,89 @@ export class StatisticService {
       selectedTransactionByDate,
       totalExpenses,
     );
+  }
+  /*[
+    {
+        "_id": "66c0dc9f988db91247485508",
+        "transactionType": "Income",
+        "title": "Some title",
+        "categories": "Rent",
+        "amount": 3,
+        "paymentDate": "2012-01-01T00:00:00.000Z",
+        "payee": "25",
+        "description": "some description",
+        "accountId": "66ba5ec4702f1db2592bf94c",
+        "uploadedFiles": []
+    },
+    {
+        "_id": "66c1d261527ef998f25e6342",
+        "transactionType": "Expenses",
+        "title": "Some title",
+        "categories": "House",
+        "amount": 85,
+        "paymentDate": "2012-02-12T00:00:00.000Z",
+        "payee": "Bob",
+        "description": "some description",
+        "accountId": "66ba5ec4702f1db2592bf94c",
+        "uploadedFiles": []
+    },*/
+  async retrieveMonthlyStatistic(body: RetrieveMonthlyStatisticDto) {
+    return this.transactionModel.aggregate([
+      {
+        $match: {
+          accountId: body.accountId,
+          paymentDate: {
+            $gte: new Date(body.startDate),
+            $lte: new Date(body.endDate),
+          },
+        },
+      },
+      {
+        $sort: {
+          paymentDate: 1,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: '%B-%Y',
+              date: '$paymentDate',
+            },
+          },
+          income: {
+            $sum: {
+              $cond: {
+                if: { $eq: ['$transactionType', TransactionType.INCOME] },
+                then: '$amount',
+                else: 0,
+              },
+            },
+          },
+          expenses: {
+            $sum: {
+              $cond: {
+                if: { $eq: ['$transactionType', TransactionType.EXPENSES] },
+                then: '$amount',
+                else: 0,
+              },
+            },
+          },
+          total: {
+            $sum: '$amount',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          month: '$_id',
+          total: 1,
+          income: 1,
+          expenses: 1,
+        },
+      },
+    ]);
   }
 
   private categorizedAmountsUnder(
