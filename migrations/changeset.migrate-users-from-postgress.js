ca4f57046705c9046efdca9199e9050c9118da2f
siarhkul <siarhkul@gmail.com>
2024-09-06 10:47:20 +0200

    chore: Add manual scripts2

--- migrations/changeset.migrate-users-from-postgress.js
+++ migrations/changeset.migrate-users-from-postgress.js
@@ -1,16 +1,16 @@
 const { retrieveDataFromPostgres } = require('../setup/postgres.setup');
+const { mapUserPGDBtoUserMongoDB } = require('../src/migrations-manual/migrations-manual');
 
-// const { mapUserPGDBtoUserMongoDB } = require('../src/migrations-manual/migrations-manual.mapper');
 
 const pgQuery = 'SELECT * FROM users';
 
 module.exports = {
   async up(db, client) {
 
-    // const res = await retrieveDataFromPostgres(pgQuery);
-    // const userMongoDb = mapUserPGDBtoUserMongoDB(res);
-    // console.log('Postgres data========================:', res);
-    // console.log('mapped data --------------------', userMongoDb);
+    const res = await retrieveDataFromPostgres(pgQuery);
+    const userMongoDb = mapUserPGDBtoUserMongoDB(res);
+    console.log('Postgres data========================:', res);
+    console.log('mapped data --------------------', userMongoDb);
   },
 
   async down(db, client) {
