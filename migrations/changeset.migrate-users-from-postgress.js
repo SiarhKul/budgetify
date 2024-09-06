d0b4252553dd7f3dfb4106e3ff3548d3da84a131
siarhkul <siarhkul@gmail.com>
2024-09-06 11:30:50 +0200

    chore: Add Jsdoc for the types

--- migrations/changeset.migrate-users-from-postgress.js
+++ migrations/changeset.migrate-users-from-postgress.js
@@ -1,14 +1,28 @@
 const { retrieveDataFromPostgres } = require('../setup/postgres.setup');
-const { mapUserPGDBtoUserMongoDB } = require('../src/migrations-manual/migrations-manual');
-
 
 const pgQuery = 'SELECT * FROM users';
+
+/**
+ * Maps an array of Postgres users to MongoDB users format.
+ *
+ * @param {Array<Object>} pgUsers - The array of users from the Postgres database.
+ * @param {Object} pgUsers[].email - The email of the Postgres user.
+ * @param {Object} pgUsers[].password - The password of the Postgres user.
+ * @returns {Array<Object>} - The array of users formatted for MongoDB.
+ */
+const mapToMongoDbUsers = (pgUsers) => {
+  return pgUsers.map((userFromPGDB) => ({
+    email: userFromPGDB.email,
+    password: userFromPGDB.password,
+  }));
+};
 
 module.exports = {
   async up(db, client) {
 
-    const res = await retrieveDataFromPostgres(pgQuery);
-    const userMongoDb = mapUserPGDBtoUserMongoDB(res);
+    const pgUsers = await retrieveDataFromPostgres(pgQuery);
+    const mongoDbUsers =mapToMongoDbUsers(pgUsers)
+
     console.log('Postgres data========================:', res);
     console.log('mapped data --------------------', userMongoDb);
   },
